3c33c1d129b0bc5bf43e646c9e148a12170589a6
Yashit <veeryashit@gmail.com>
2022-05-12 12:24:04 +0530

    Init

---
+++ Github-API/github.js
@@ -0,0 +1,26 @@
+class GitHub {
+  constructor() {
+    this.client_id = "59e1c2fafa2accb77f00";
+    this.client_secret = "a5266e829d11a90ae0cad4a3b504b1535b9c409c";
+    this.repos_count = 5;
+    this.repos_sort = "created: asc";
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
+    const reposResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
+    const profile = await profileResponse.json();
+    const repos = await reposResponse.json();
+
+    return {
+      profile,
+      repos,
+    };
+  }
+}
